f682c02a8d4e82c980275e46b0245d526c1d6ca4
Layonj3000 <layon.reis@edu.ufes.br>
2024-07-21 15:16:41 -0300

    :sparkles: feat: adicionando configurações do banco de dados

--- src/chatbot/scripts/lambda function/lambda_function.py
+++ src/chatbot/scripts/lambda function/lambda_function.py
@@ -1,3 +1,11 @@
 import pymysql
 import json
 import re
+
+# Configurações do banco de dados
+db_config = {
+    'host': 'HOST',
+    'user': 'USER',
+    'password': 'PASSWORD',
+    'database': 'DATABASE'
+}
