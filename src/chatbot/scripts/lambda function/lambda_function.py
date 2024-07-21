360c6fd3debd747ada23d4bfc14bb232dc923c2f
Luiz Fillipe <luiz.fillipe.2000@hotmail.com>
2024-07-21 16:00:37 -0300

    :sparkles: feat: convertendo dados para para formatação correta

--- src/chatbot/scripts/lambda function/lambda_function.py
+++ src/chatbot/scripts/lambda function/lambda_function.py
@@ -140,6 +140,27 @@ def lambda_handler(event, context):
     
             connection.commit()
             print("Consulta realizada com sucesso")
+
+             # Converter datetime para string no formato brasileiro
+            def format_datetime_br(datetime_str):
+                from datetime import datetime
+                dt = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
+                return dt.strftime('%d/%m/%Y %H:%M:%S')
+    
+            appointments = [(appointment[0], appointment[1], appointment[2], format_datetime_br(appointment[3].strftime('%Y-%m-%d %H:%M:%S'))) for appointment in appointments]
+            print("Consultas formatadas:", appointments)
+    
+            # Criar a mensagem em formato de tabela com colunas alinhadas
+            message = "Suas consultas:\n"
+            message += "{:<16} | {:<10} | {:<15} | {:<20}\n".format("ID da Consulta", "ID do Pet", "Nome do Pet", "Data da Consulta")
+            message += "-" * 65 + "\n"
+            for appointment in appointments:
+                message += "{:<25} | {:<17} | {:<15} | {:<20}\n".format(appointment[0], appointment[1], appointment[2], appointment[3])
+    
+            return {
+                'statusCode': 200,
+                'body': json.dumps(message)
+            }
         except pymysql.MySQLError as e:
             print(f"Erro ao buscar consultas no banco de dados: {str(e)}")
             return {
