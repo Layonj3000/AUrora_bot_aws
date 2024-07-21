9afd0a5f4049df166f000efe1672bc3baa1fabd8
Layonj3000 <layon.reis@edu.ufes.br>
2024-07-21 15:26:23 -0300

    :sparkles: feat: adicionando a função elicit_slot

--- src/chatbot/scripts/lambda function/lambda_function.py
+++ src/chatbot/scripts/lambda function/lambda_function.py
@@ -12,6 +12,25 @@ db_config = {
 
 def lambda_handler(event, context):
 
+    def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message):
+        print(f"Elicit slot {slot_to_elicit} with message: {message}")
+        return {
+            'sessionState': {
+                'dialogAction': {
+                    'type': 'ElicitSlot',
+                    'slotToElicit': slot_to_elicit
+                },
+                'intent': {
+                    'name': intent_name,
+                    'slots': slots
+                }
+            },
+            'messages': [{
+                'contentType': 'PlainText',
+                'content': message
+            }]
+        }
+    
     return {
             'statusCode': 200,
             'body': json.dumps("Hello from Lambda")
