d8470a28a2ef8af2cd27a7c46c5c3e51b94d71ea
Luiz Fillipe <luiz.fillipe.2000@hotmail.com>
2024-07-21 16:02:04 -0300

    :sparkles: feat: crianção da função cancel_appointment

--- src/chatbot/scripts/lambda function/lambda_function.py
+++ src/chatbot/scripts/lambda function/lambda_function.py
@@ -171,6 +171,35 @@ def lambda_handler(event, context):
             if connection:
                 connection.close()
                 print("Conexão fechada")
+
+
+    def cancel_appointment(data):
+        print("cancel_appointment with data:", data)
+        print(f"Email: {data['email']}, Pet ID: {data['pet_id']}, Appointment ID: {data['appointment_id']}")
+
+        try:
+            connection = pymysql.connect(**db_config)
+            with connection.cursor() as cursor:
+                # Cancelar consulta
+                query = '''
+                DELETE a 
+                FROM appointments a
+                JOIN pets p ON a.pet_id = p.pet_id
+                WHERE a.customer_email = %s AND p.pet_id = %s AND a.appointment_id = %s
+                '''
+                cursor.execute(query, (data['email'], data['pet_id'], data['appointment_id']))
+
+            connection.commit()
+
+        except pymysql.MySQLError as e:
+            print(f"Erro ao cancelar consulta no banco de dados: {str(e)}")
+            return {
+                'statusCode': 500,
+                'body': json.dumps(f"Erro ao cancelar consulta no banco de dados: {str(e)}")
+            }
+        finally:
+            connection.close()
+
     return {
             'statusCode': 200,
             'body': json.dumps("Hello from Lambda")
