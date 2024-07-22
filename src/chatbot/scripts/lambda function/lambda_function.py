12a95ed95f47a5052dc38d567ddf332472f880dc
Luiz Fillipe <luiz.fillipe.2000@hotmail.com>
2024-07-22 13:52:44 -0300

    :ok_hand: style: Alterações de revisão de código

--- src/chatbot/scripts/lambda function/lambda_function.py
+++ src/chatbot/scripts/lambda function/lambda_function.py
@@ -7,17 +7,15 @@ from urllib.error import HTTPError, URLError
 import boto3
 import pymysql
 from botocore.exceptions import ClientError
-from cancel_appointment import cancel_appointment
-from fetch_appointments import fetch_appointments
-from insert_into_db import insert_into_db
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
+from cancel_appointment import cancel_appointment
+from fetch_appointments import fetch_appointments
+from insert_into_db import insert_into_db
+
 # Configurar os clientes boto3
 s3 = boto3.client('s3')
-slack_client = WebClient(token="xoxb-7440134265859-7442296449205-yHdYhT1cISAT9Yx7jsU0k0e8")
-lex_client = boto3.client('lexv2-models')
-
 
 # Configurações do banco de dados
 db_config = {
@@ -29,7 +27,7 @@ db_config = {
 
 def generate_audio(phrase):
     print("generate_audio(phrase)",phrase)
-    api_url = "https://ot9mztyeff.execute-api.us-east-1.amazonaws.com/v1/tts"
+    api_url = "APU_URL"
     headers = {
         'Content-Type': 'application/json',
     }
@@ -44,25 +42,6 @@ def generate_audio(phrase):
 
     audio_url = data['url_to_audio']
     return audio_url
-
-def send_audio_to_slack(audio_url):
-    file_name = '/tmp/audio.mp3'
-    with urllib.request.urlopen(audio_url) as audio_response:
-        if audio_response.status != 200:
-            raise HTTPError(audio_response.url, audio_response.status, audio_response.msg, audio_response.headers, None)
-        with open(file_name, 'wb') as f:
-            f.write(audio_response.read())
-
-    slack_response = slack_client.files_upload_v2(
-        channels="D07CNJTJNLX",  # Verifique se o ID do canal está correto
-        file=file_name,
-        title="Áudio Gerado",
-        initial_comment="Aqui está o áudio gerado."
-    )
-    if not slack_response["ok"]:
-        raise Exception(f"Slack API error: {slack_response['error']}")
-
-    return slack_response
 
 
 def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message):
@@ -85,7 +64,7 @@ def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message)
     }
 
 def delegate(intent_request):
-    print("delegate called")
+
     return {
         'sessionState': {
             'dialogAction': {
@@ -96,7 +75,7 @@ def delegate(intent_request):
     }
     
 def close_with_message(intent_request, fulfillment_state, message, message2):
-    print("close called with message:", message)
+
     return {
         'sessionState': {
             'dialogAction': {
@@ -119,7 +98,7 @@ def close_with_message(intent_request, fulfillment_state, message, message2):
         ]
     }
 def close(intent_request, fulfillment_state, message):
-    print("close called with message:", message)
+
     return {
         'sessionState': {
             'dialogAction': {
@@ -152,8 +131,7 @@ def lambda_handler(event, context):
     try:
         intent_name = event['sessionState']['intent']['name']
         slots = event['sessionState']['intent']['slots']
-        print("Intent name:", intent_name)
-        print("Slots:", json.dumps(slots))
+
 
         if intent_name == 'AgendarConsulta':
             
@@ -167,15 +145,12 @@ def lambda_handler(event, context):
                     'data': slots['data']['value']['interpretedValue'],
                     'horario': slots['horario']['value']['interpretedValue']
                 }
-                print("Preparando para inserir no banco de dados")
+  
                 
                 email_novo = remove_mailto(data['email'])
                 if is_valid_email(email_novo):
-                    print("Email validado:", email_novo)
                     data['email'] = email_novo
                 else:
-                    print("Email inválido:", email_novo)
-
                     return elicit_slot(event['sessionState']['sessionAttributes'], intent_name, slots, 'email', f' O e-mail digitado não é válido. Insira um e-mail válido.')
                     
                 response = insert_into_db(data)
@@ -198,11 +173,8 @@ def lambda_handler(event, context):
                 email_novo = remove_mailto(data['email'])
                 
                 if is_valid_email(email_novo):
-                    print("Email validado:", email_novo)
                     data['email'] = email_novo
                 else:
-                    print("Email inválido:", email_novo)
-
                     return elicit_slot(event['sessionState']['sessionAttributes'], intent_name, slots, 'email', f'Insira um email válido.')
                 
                 response = fetch_appointments(data)
@@ -232,10 +204,8 @@ def lambda_handler(event, context):
                     email_novo = remove_mailto(data['email'])
                     
                     if is_valid_email(email_novo):
-                        print("Email validado:", email_novo)
                         data['email'] = email_novo
                     else:
-                        print("Email inválido:", email_novo)
                         return elicit_slot(event['sessionState']['sessionAttributes'], intent_name, slots, 'email', f'Insira um email válido.')
                         
                     response = cancel_appointment(data)
@@ -261,7 +231,6 @@ def lambda_handler(event, context):
         return close(event, 'Failed', 'Erro ao processar a intenção.')
                 
     except Exception as e:
-        print(f"Erro geral no Lambda: {str(e)}")
         return close(event, 'Failed', f"Erro ao processar a intenção: {str(e)}")
     
     
