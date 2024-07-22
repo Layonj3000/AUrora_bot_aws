091c4724304adae871655ce033bc300fad41ede0
Luiz Fillipe <luiz.fillipe.2000@hotmail.com>
2024-07-22 13:36:08 -0300

    :sparkles: feat: criação da função para enviar o audio

--- src/chatbot/scripts/lambda function/lambda_function.py
+++ src/chatbot/scripts/lambda function/lambda_function.py
@@ -1,10 +1,23 @@
 import json
+import os
 import re
+import urllib.request
+from urllib.error import HTTPError, URLError
 
+import boto3
 import pymysql
+from botocore.exceptions import ClientError
 from cancel_appointment import cancel_appointment
 from fetch_appointments import fetch_appointments
 from insert_into_db import insert_into_db
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+# Configurar os clientes boto3
+s3 = boto3.client('s3')
+slack_client = WebClient(token="xoxb-7440134265859-7442296449205-yHdYhT1cISAT9Yx7jsU0k0e8")
+lex_client = boto3.client('lexv2-models')
+
 
 # Configurações do banco de dados
 db_config = {
@@ -13,6 +26,44 @@ db_config = {
     'password': 'PASSWORD',
     'database': 'DATABASE'
 }
+
+def generate_audio(phrase):
+    print("generate_audio(phrase)",phrase)
+    api_url = "https://ot9mztyeff.execute-api.us-east-1.amazonaws.com/v1/tts"
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    data = json.dumps({'phrase': phrase}).encode('utf-8')
+    req = urllib.request.Request(api_url, data=data, headers=headers)
+
+    with urllib.request.urlopen(req) as response:
+        if response.status != 200:
+            raise HTTPError(response.url, response.status, response.msg, response.headers, None)
+        response_data = response.read()
+        data = json.loads(response_data.decode('utf-8'))
+
+    audio_url = data['url_to_audio']
+    return audio_url
+
+def send_audio_to_slack(audio_url):
+    file_name = '/tmp/audio.mp3'
+    with urllib.request.urlopen(audio_url) as audio_response:
+        if audio_response.status != 200:
+            raise HTTPError(audio_response.url, audio_response.status, audio_response.msg, audio_response.headers, None)
+        with open(file_name, 'wb') as f:
+            f.write(audio_response.read())
+
+    slack_response = slack_client.files_upload_v2(
+        channels="D07CNJTJNLX",  # Verifique se o ID do canal está correto
+        file=file_name,
+        title="Áudio Gerado",
+        initial_comment="Aqui está o áudio gerado."
+    )
+    if not slack_response["ok"]:
+        raise Exception(f"Slack API error: {slack_response['error']}")
+
+    return slack_response
+
 
 def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message):
     print(f"Elicit slot {slot_to_elicit} with message: {message}")
@@ -43,26 +94,53 @@ def delegate(intent_request):
             'intent': intent_request['sessionState']['intent']
         }
     }
-
-def close(intent_request, fulfillment_state, message):
-        print("close called with message:", message)
-        return {
-            'sessionState': {
-                'dialogAction': {
-                    'type': 'Close'
-                },
-                'intent': {
-                    'name': intent_request['sessionState']['intent']['name'],
-                    'state': fulfillment_state
-                }
+    
+def close_with_message(intent_request, fulfillment_state, message, message2):
+    print("close called with message:", message)
+    return {
+        'sessionState': {
+            'dialogAction': {
+                'type': 'Close'
             },
-            'messages': [{
+            'intent': {
+                'name': intent_request['sessionState']['intent']['name'],
+                'state': fulfillment_state
+            }
+        },
+        'messages': [
+            {
                 'contentType': 'PlainText',
                 'content': message
-            }]
-        }
+            },
+            {
+                'contentType': 'PlainText',
+                'content': message2
+            }
+        ]
+    }
+def close(intent_request, fulfillment_state, message):
+    print("close called with message:", message)
+    return {
+        'sessionState': {
+            'dialogAction': {
+                'type': 'Close'
+            },
+            'intent': {
+                'name': intent_request['sessionState']['intent']['name'],
+                'state': fulfillment_state
+            }
+        },
+        'messages': [
+            {
+                'contentType': 'PlainText',
+                'content': message
+            }
+        ]
+    }
+def lambda_handler(event, context):
+
     
-def lambda_handler(event, context):  
+    
     try:
         intent_name = event['sessionState']['intent']['name']
         slots = event['sessionState']['intent']['slots']
@@ -103,9 +181,11 @@ def lambda_handler(event, context):
                     
                 response = insert_into_db(data)
                 if response['statusCode'] == 200:
-                    return close(event, 'Fulfilled', 'Consulta marcada com sucesso')
+                    audio_url = generate_audio('Consulta marcada com sucesso')
+                    return close_with_message(event, 'Fulfilled', 'Consulta marcada com sucesso!',audio_url)
                 else:
-                    return close(event, 'Failed', 'Erro ao inserir os dados no banco de dados.')
+                    audio_url = generate_audio('Erro ao inserir os dados no banco de dados.')
+                    return close_with_message(event, 'Failed', 'Erro ao inserir os dados no banco de dados.',audio_url)
             else:
                 return delegate(event)
             
@@ -127,17 +207,22 @@ def lambda_handler(event, context):
                     return elicit_slot(event['sessionState']['sessionAttributes'], intent_name, slots, 'email', f'Insira um email válido.')
                 
                 response = fetch_appointments(data)
+               
                 if response['statusCode'] == 200:
+                    audio_url = generate_audio('Consulta realizada com sucesso!')
                     message = json.loads(response['body'])
-                    return close(event, 'Fulfilled', message)
+                    return close_with_message(event, 'Fulfilled', message,audio_url)
                 elif response['statusCode'] == 404:
-                    return close(event, 'Failed', 'Nenhuma consulta encontrada para o email fornecido.')
+                    audio_url = generate_audio('Nenhuma consulta encontrada para o email fornecido.')
+                    return close_with_message(event, 'Failed', 'Nenhuma consulta encontrada para o email fornecido.',audio_url)
                 else:
                     return close(event, 'Failed', 'Erro ao buscar as consultas no banco de dados.')
 
             else:
                 return delegate(event)
         if intent_name == 'DesmarcarConsulta':
+
+
                 if event['invocationSource'] == 'FulfillmentCodeHook':
                     data = {
                         'email': slots['email']['value']['interpretedValue'],
@@ -154,16 +239,25 @@ def lambda_handler(event, context):
                         print("Email inválido:", email_novo)
                         return elicit_slot(event['sessionState']['sessionAttributes'], intent_name, slots, 'email', f'Insira um email válido.')
                         
-                    
                     response = cancel_appointment(data)
+                    
+
                     if response['statusCode'] == 200:
-                        return close(event, 'Fulfilled', 'Consulta cancelada com sucesso')
+                        audio_url = generate_audio('Consulta cancelada com sucesso!')
+                        return close_with_message(event, 'Fulfilled', 'Consulta cancelada com sucesso! \n', audio_url)
                     elif response['statusCode'] == 404:
-                        return close(event, 'Failed', 'Consulta não encontrada.')
+                        audio_url = generate_audio('Consulta não encontrada.')
+                        return close_with_message(event, 'Failed', 'Consulta não encontrada.',audio_url)
                     else:
                         return close(event, 'Failed', 'Erro ao cancelar a consulta no banco de dados.')
                 else:
                     return delegate(event)
+             
+             
+        if intent_name == 'Localizacao':
+            audio_url = generate_audio('Avenida Dálmata, nº 101, Bairro Zeca Urubu, Cidade de Garfield!')
+            return close(event, 'Fulfilled', audio_url)
+        
                 
         return close(event, 'Failed', 'Erro ao processar a intenção.')
                 
